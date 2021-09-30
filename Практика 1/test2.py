b3f88c4e0816a427c9c9af7f80e9ee22aceca508
bufebaa <71431563+bufebaa@users.noreply.github.com>
2021-09-30 18:48:55 +0300

    Add files via upload

---
+++ Практика 1/test2.py
@@ -0,0 +1,39 @@
+
+class Rectangle:
+
+    def __init__(self):
+        self.__width = 1.0
+        self.__length = 1.0
+
+    def count_perimiter(self):
+        return self.__length*2+self.__width*2
+
+    def count_area(self):
+        return self.__length*self.__width
+
+    @property
+    def length(self):
+        return self.__length
+
+    @property
+    def width(self):
+        return self.__width
+
+    @length.setter
+    def length(self, length):
+        if (0.0<length<20.0) and type(length) is float:
+            self.__length=length
+        else:
+            print("Invalid numbers")
+
+    @width.setter
+    def width(self, width):
+        if (0.0<width<20.0)  and type(width) is float:
+            self.__width=width
+
+
+
+rectangle1 = Rectangle()
+rectangle1.width = 2.36
+rectangle1.length = 6.32
+print(rectangle1.count_area())
